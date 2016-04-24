9cced7f00b56ec594e49c5800f9665ff3cc556bb
Scott Diallo <diallot@me.com>
2016-04-24 16:22:53 -0700

    minor changes

--- js/app.js
+++ js/app.js
@@ -4,9 +4,8 @@ $(document).ready(function() {
     var guess;
     var oldGuess = 0;
     var userGuess;
-	var guessList;
-	var guessHmtl;
-    var counter = [];
+    var guessList;
+    var counter =[];
 
     // start a new game
     newGame();
@@ -37,69 +36,47 @@ $(document).ready(function() {
         newGame();
     });
     $("#guessButton").click(function() {
-		checkGuess();
-
+        checkGuess();
     });
-//convert userguess to integer and generate feedback based on guess
-	function checkGuess() {
-		var userGuess = parseInt($("#userGuess").val());
-		verifyNumber(userGuess);
+    //convert userguess to integer and generate feedback based on guess
+    function checkGuess() {
+        var userGuess = parseInt($("#userGuess").val());
+        verifyNumber(userGuess);
         guessHistory(userGuess);
         showGuessCounter(counter);
-		console.log(userGuess);
-		console.log(secretNumber);
-		if (userGuess === secretNumber){
-			$('#feedback').text('You nailed it! click New Game to start a new game');
-		}
-		else if (userGuess >= secretNumber -10 && userGuess <= secretNumber +10 ) {
-			$('#feedback').text('Very hot!');
-		}
-		else if (userGuess >= secretNumber -20 && userGuess <= secretNumber +20 ) {
-			$('#feedback').text('Hot!');
-		}
-		else if (userGuess >= secretNumber -30 && userGuess <= secretNumber +30 ) {
-			$('#feedback').text('warm!');
-		}
-		else if (userGuess >= secretNumber -50 && userGuess <= secretNumber +50 ) {
-			$('#feedback').text('Cold!');
-		}
-		else  {
-			$('#feedback').text('Ice Cold!');
-	}
-	function verifyNumber(userGuess){
-		 if (userGuess < 0 || userGuess > 100){
-			 alert('you must type a number between 1 - 100');
-		 }
-	 }
+        console.log(userGuess);
+        console.log(secretNumber);
+        if (userGuess === secretNumber) {
+            $('#feedback').text('You nailed it! click New Game to play again');
+        } else if (userGuess >= secretNumber - 10 && userGuess <= secretNumber + 10) {
+            $('#feedback').text('Very hot!');
+        } else if (userGuess >= secretNumber - 20 && userGuess <= secretNumber + 20) {
+            $('#feedback').text('Hot!');
+        } else if (userGuess >= secretNumber - 30 && userGuess <= secretNumber + 30) {
+            $('#feedback').text('warm!');
+        } else if (userGuess >= secretNumber - 50 && userGuess <= secretNumber + 50) {
+            $('#feedback').text('Cold!');
+        } else {
+            $('#feedback').text('Ice Cold!');
+        }
 
-// tracking guesses and listing them
-// function trackGuess(){
-// 	pastGuess[0].push(userGuess);
-// 	guessHmtl = '';
-// 	if(pastGuess[0].length){
-// 		$.each(pastGuess,
-// 			function(guess,value){
-// 			guessHmtl += '<li>' + value + '</li>';
-// 		});
-// 	}
-// }
-//
-//
-// function guessCount(){
-// 	count++;
-// }
-// count the number of guesses
-function showGuessCounter(counter) {
-     $('#count').text(counter);
-        counter++;
-}
+        function verifyNumber(userGuess) {
+            if (userGuess < 0 || userGuess > 100) {
+                alert('you must type a number between 1 - 100');
+            }
+        }
+// function to display number of guesses users has entered
+        function showGuessCounter(userGuess) {
+            $('#count').text(counter);
+            counter++;
+        }
 
-//show history of all the guesses
-function guessHistory(userGuess) {
-    $('#guessList').append('<li>' + userGuess + '</li>');
-}
+        //show history of all the guesses
+        function guessHistory(userGuess) {
+            $('#guessList').append('<li>' + userGuess + '</li>');
+        }
 
-	}
+    }
     newGame();
 
     function newGame() {
