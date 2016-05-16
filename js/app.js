edfedcadb9ca898020cf502100ed5c9b827cfe5a
Scott Diallo <diallot@me.com>
2016-05-16 15:01:04 -0700

    nAn function created when non-number is entered

--- js/app.js
+++ js/app.js
@@ -1,86 +1,84 @@
-$(document).ready(function() {
+//empty input field after guess btn click
+//alert for input than are not numbers
+//
+
+$(document).ready(function () {
     //Global variables declarations
     var secretNumber = Math.floor(Math.random() * 100) + 1;
     var guess;
     var oldGuess = 0;
     var userGuess;
     var guessList;
-    var counter =[];
+    var counter = 0;
 
     // start a new game
-    newGame();
 
-    $('form').submit(function(event) {
-        event.preventDefault();
 
-        if (!found) {
-            userGuess = $('#userGuess').val();
-            console.log('userGuess = ' + userGuess);
-            clearText();
-            setFocus();
-        }
-    });
 
     /*--- Display information modal box ---*/
-    $(".what").click(function() {
+    $(".what").click(function () {
         $(".overlay").fadeIn(1000);
 
     });
 
     /*--- Hide information modal box ---*/
-    $("a.close").click(function() {
+    $("a.close").click(function () {
         $(".overlay").fadeOut(1000);
     });
 
-    $(".new").click(function() {
+    $(".new").click(function () {
         newGame();
     });
-    $("#guessButton").click(function() {
-        checkGuess();
+    $("#guessButton").click(function () {
+        var userGuess = $("#userGuess").val();
+        checkGuess(userGuess);
+        $('#userGuess').val(''); //reset input field when btn's press
+
     });
     //convert userguess to integer and generate feedback based on guess
-    function checkGuess() {
-        var userGuess = parseInt($("#userGuess").val());
-        verifyNumber(userGuess);
-        guessHistory(userGuess);
-        showGuessCounter(counter);
-        console.log(userGuess);
-        console.log(secretNumber);
-        if (userGuess === secretNumber) {
-            $('#feedback').text('You nailed it! click New Game to play again');
-        } else if (userGuess >= secretNumber - 10 && userGuess <= secretNumber + 10) {
-            $('#feedback').text('Very hot!');
-        } else if (userGuess >= secretNumber - 20 && userGuess <= secretNumber + 20) {
-            $('#feedback').text('Hot!');
-        } else if (userGuess >= secretNumber - 30 && userGuess <= secretNumber + 30) {
-            $('#feedback').text('warm!');
-        } else if (userGuess >= secretNumber - 50 && userGuess <= secretNumber + 50) {
-            $('#feedback').text('Cold!');
+    function checkGuess(userGuess) {
+        if (isNaN(userGuess)) {
+            alert('Come on yooo ENTER a number');
+        } else if (userGuess % 1 !== 0) { //avoid decimal num
+            alert('No decimal number');
+        } else if (userGuess < 1 || userGuess > 100) {
+            alert('you must type a number between 1 - 100');
         } else {
-            $('#feedback').text('Ice Cold!');
-        }
 
-        function verifyNumber(userGuess) {
-            if (userGuess < 0 || userGuess > 100) {
-                alert('you must type a number between 1 - 100');
+            guessHistory(userGuess);
+            counter++;
+            showGuessCounter(counter);
+            console.log(userGuess);
+            console.log(secretNumber);
+
+            if (userGuess === secretNumber) {
+                $('#feedback').text('You nailed it! click New Game to play again');
+            } else if (userGuess >= secretNumber - 10 && userGuess <= secretNumber + 10) {
+                $('#feedback').text('Very hot!');
+            } else if (userGuess >= secretNumber - 20 && userGuess <= secretNumber + 20) {
+                $('#feedback').text('Hot!');
+            } else if (userGuess >= secretNumber - 30 && userGuess <= secretNumber + 30) {
+                $('#feedback').text('warm!');
+            } else if (userGuess >= secretNumber - 50 && userGuess <= secretNumber + 50) {
+                $('#feedback').text('Cold!');
+            } else {
+                $('#feedback').text('Ice Cold!');
             }
         }
-// function to display number of guesses users has entered
-        function showGuessCounter(userGuess) {
-            $('#count').text(counter);
-            counter++;
-        }
+    };
 
-        //show history of all the guesses
-        function guessHistory(userGuess) {
-            $('#guessList').append('<li>' + userGuess + '</li>');
-        }
 
+
+
+
+    // function to display number of guesses users has entered
+    function showGuessCounter(counter) {
+        $('#count').text(counter);
     }
-    newGame();
 
-    function newGame() {
-
-
+    //show history of all the guesses
+    function guessHistory(userGuess) {
+        $('#guessList').append('<li>' + userGuess + '</li>');
     }
+
 });
